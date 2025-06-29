651dbfac16b958c08f2beed48f23373c1fedc9ce
LEXAMETA <lexagreeter@gmail.com>
2025-06-29 17:50:00 -0500

    Update Characters.ts

--- lib/state/Characters.ts
+++ lib/state/Characters.ts
@@ -895,6 +895,7 @@ export const createChat = async (charId: number) => {
     }, [data, id, updateCard]); // Added all dependencies for correctness
 };
 
+
 const characterCardV1Schema = z.object({
     name: z.string(),
     description: z.string(),
@@ -959,30 +960,4 @@ const createBlankV2Card = (
             character_version: '',
         },
     };
-};
-
-type Macro = {
-    macro: string;
-    value: string;
-};
-
-const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-export const replaceMacros = (text: string) => {
-    if (text === undefined) return '';
-    let newtext: string = text;
-    const charName = Characters.useCharacterCard.getState().card?.name ?? '';
-    const userName = Characters.useUserCard.getState().card?.name ?? '';
-    const time = new Date();
-    const rules: Macro[] = [
-        { macro: '{{user}}', value: userName },
-        { macro: '{{char}}', value: charName },
-        { macro: '{{time}}', value: time.toLocaleTimeString() },
-        { macro: '{{date}}', value: time.toLocaleDateString() },
-        { macro: '{{day}}', value: weekday[time.getDay()] },
-    ];
-    for (const rule of rules) newtext = newtext.replaceAll(rule.macro, rule.value);
-    return newtext;
-};
-
-// Close Characters namespace here if not already closed
-    }
+};        
